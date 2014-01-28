964b619b8dbea1760559f8fd7987c31edc7c870c
jmdcodejunkie <jmdcodejunkie@gmail.com>
2014-01-28 19:54:05 +0000

    Updates to site, to move off of functionality and allow for hardcoding website information.

--- EQNWebsiteUI/Scripts/jQuery-Custom/initialize.js
+++ EQNWebsiteUI/Scripts/jQuery-Custom/initialize.js
@@ -1,14 +1,9 @@
-﻿
+﻿var gridster;
 $(function () {
     $('.menu-list').menubar();
 
-    $('.jdWidget-wrapper').gridster({
-        widget_margins: [10, 10],
-        widget_base_dimensions: [140, 140],
-        widget_selector: "div.jdWidget-wrapper",
-        autogenerate_stylesheet: false,
-        draggable: {
-            handle: "jdWidget-titlebar-handle"
-        }
-    });
+    gridster = $('.widget-area').gridster({
+        widget_margins: [3, 3],
+        widget_base_dimensions: [150, 75]
+    }).data("gridster");
 });
