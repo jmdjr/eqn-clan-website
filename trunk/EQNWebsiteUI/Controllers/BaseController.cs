80e1cb9602a52129600b8994eedf24b46bcdb4ac
jmdcodejunkie@gmail.com <jmdcodejunkie@gmail.com@b4f65799-b842-cd9d-5635-654fd2953b62>
2014-02-20 00:23:05 +0000

    added additional classes to allow for caching and Server side css feeds.

--- trunk/EQNWebsiteUI/Controllers/BaseController.cs
+++ trunk/EQNWebsiteUI/Controllers/BaseController.cs
@@ -7,32 +7,45 @@ using System.Web.Mvc;
 using System.Xml;
 using System.Xml.Serialization;
 using EQNWebsiteUI.Models;
+using EQNWebsiteUI.Classes;
 
 namespace EQNWebsiteUI.Controllers
 {
     public class BaseController : Controller
     {
+        private object xmlLock = new object();
+
         private T StreamXML<T>(string xmlUrl)
         {
-            FileStream stream = new FileStream(Server.MapPath(xmlUrl), FileMode.Open);
-            XmlReader reader = new XmlTextReader(stream);
-            XmlSerializer serial = new XmlSerializer(typeof(T));
             T Model = default(T);
 
-            try
+            lock (xmlLock)
             {
-                if (serial.CanDeserialize(reader))
+                XmlReader reader = null;
+                try
                 {
-                    Model = (T)serial.Deserialize(reader);
-                }
-            }
-            catch (Exception)
-            {
+                    using (FileStream stream = new FileStream(Server.MapPath(xmlUrl), FileMode.Open))
+                    {
+                        reader = new XmlTextReader(stream);
+                        XmlSerializer serial = new XmlSerializer(typeof(T));
 
-            }
-            finally
-            {
-                reader.Close();
+                        if (serial.CanDeserialize(reader))
+                        {
+                            Model = (T)serial.Deserialize(reader);
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+
+                }
+                finally
+                {
+                    if (reader != null)
+                    {
+                        reader.Close();
+                    }
+                }
             }
 
             return Model;
@@ -40,21 +53,48 @@ namespace EQNWebsiteUI.Controllers
 
         public PartialViewResult MainMenu() 
         {
+            // update cache if cache has expired/non-existing items
+            if (!ObjectCacheManager<MenuModel>.Instance.Contains("RegisteredMenu"))
+            {
+                ObjectCacheManager<MenuModel>.Instance.Add("RegisteredMenu", StreamXML<MenuModel>("/Content/Menus/RegisteredUserMenu.xml"));
+            }
+            if (!ObjectCacheManager<MenuModel>.Instance.Contains("AnonymousMenu"))
+            {
+                ObjectCacheManager<MenuModel>.Instance.Add("AnonymousMenu", StreamXML<MenuModel>("/Content/Menus/AnonymousUserMenu.xml"));
+            }
 
             if (User.Identity.IsAuthenticated)
             {
-                ViewData["Menu"] = StreamXML<MenuModel>("/Content/Menus/RegisteredUserMenu.xml");
+                ViewData["Menu"] = ObjectCacheManager<MenuModel>.Instance["RegisteredMenu"];
             }
             else
             {
-                ViewData["Menu"] = StreamXML<MenuModel>("/Content/Menus/AnonymousUserMenu.xml");
+                ViewData["Menu"] = ObjectCacheManager<MenuModel>.Instance["AnonymousMenu"];
             }
+
             return PartialView("_MainMenu");
         }
 
-        public void GetWidgets()
+        public void GetDemoWidgets()
         {
-            ViewData["Widgets"] = StreamXML<WidgetsCollection>("/Content/Menus/DemoHomePageWidgetLayout.xml").widgets;
+            // update cache if cache has expired/non-existing items
+            if (!ObjectCacheManager<WidgetsCollection>.Instance.Contains("Widgets"))
+            {
+                ObjectCacheManager<WidgetsCollection>.Instance.Add("Widgets", StreamXML<WidgetsCollection>("/Content/Menus/DemoHomePageWidgetLayout.xml"));
+            }
+            
+            WidgetsCollection collection = ObjectCacheManager<WidgetsCollection>.Instance["Widgets"];
+
+            if (collection != null)
+            {
+                ViewData["Widgets"] = collection.widgets;
+            }
+            else
+            {
+                ViewData["Widgets"] = null;
+            }
         }
+
+        public 
     }
 }
