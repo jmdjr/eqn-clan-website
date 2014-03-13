186065e0c61e906e2e669b2f9b18eee6909210cf
jmdcodejunkie <jmdcodejunkie@gmail.com>
2014-03-13 23:16:27 +0000

    Updates and stuff for databases. Updated database solution finally, going to write some stored procedures for it to utilize.

---
+++ EQNWebsiteDatabase/EQNWebsiteDatabase/dbo/Tables/ArticleImages.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[ArticleImages] (
+    [ArticleImageId] INT IDENTITY (1, 1) NOT NULL,
+    [ArticleId]      INT NOT NULL,
+    [ImageId]        INT NOT NULL,
+    CONSTRAINT [PK_ArticleImages] PRIMARY KEY CLUSTERED ([ArticleImageId] ASC),
+    CONSTRAINT [FK_ArticleArticleImage] FOREIGN KEY ([ArticleId]) REFERENCES [dbo].[Articles] ([ArticleId]),
+    CONSTRAINT [FK_ImageArticleImage] FOREIGN KEY ([ImageId]) REFERENCES [dbo].[Images] ([ImageId])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_FK_ImageArticleImage]
+    ON [dbo].[ArticleImages]([ImageId] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_FK_ArticleArticleImage]
+    ON [dbo].[ArticleImages]([ArticleId] ASC);
+
